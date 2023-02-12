07f8bf4cbaecbd0ea6a7c70a5eeb4a27b6f50069
AMIHbKA <bishop0013forum@gmail.com>
2023-02-12 19:13:14 +0200

    tsk#7 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,57 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
+// creating gallery items
+const galleryRef = document.querySelector(".gallery");
 
-const mainDivForGallery = document.querySelector(".gallery");
-// console.log(mainDivForGallery);
+const createGalleryItems = (galleryItems) =>
+  galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<div class="gallery__item"><a class="gallery__link" href="${original}"><img
+      class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+    )
+    .join("");
 
-const insertImgFromArrayToGallery = galleryItems
-  .map(({ preview, original, description }) => {
-    console.log(preview, original, description);
-    return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img
-      class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
-  })
-  .join("");
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+galleryRef.innerHTML = galleryItemsMarkup;
 
-mainDivForGallery.innerHTML = insertImgFromArrayToGallery;
+// event delegation
+galleryRef.addEventListener("click", onGalleryClickHandler);
 
-console.log(insertImgFromArrayToGallery);
+function onGalleryClickHandler(e) {
+  e.preventDefault();
+
+  if (e.target.tagName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+  <div class="modal">
+    <img class="modal-img" src="${e.target.dataset.source}">
+  </div>
+`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+
+  instance.show();
+
+  // Честно говоря мне такой вариант не очень нравится, но он работает. Очень бы хотелось узнать как правильно и красиво сделать закрытие по нажатию Esc
+  function onEscKeyPress(e) {
+    if (e.code.toLowerCase() !== "escape") {
+      return;
+    }
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// creating gallery items
+const galleryRef = document.querySelector(".gallery");
+
+const createGalleryItems = (galleryItems) =>
+  galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+    )
+    .join("");
+
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+galleryRef.innerHTML = galleryItemsMarkup;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function () {
+  console.log("func");
+  // do something…
+});
+
+gallery.on("error.simplelightbox", function (e) {
+  console.log(e); // some usefull information
+});
